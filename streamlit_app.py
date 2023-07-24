16a6a1ec3e2fdf0c9296dd110a52ced24a3f0fe3
rahuldodkal <140070303+rahuldodkal@users.noreply.github.com>
2023-07-24 13:18:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Healthy Diner')
 streamlit.header('Todays Menu')
 
 streamlit.text('🥣Dosa')
-streamlit.text('🥗Pasta')  
+streamlit.text('🥗Pasta')   
 streamlit.text('🐔Idli')
 streamlit.text('🥑Omlette')
 
