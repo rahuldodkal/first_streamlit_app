5e351d47a72680c4c044d6861bed87e0ce190cab
rahuldodkal <140070303+rahuldodkal@users.noreply.github.com>
2023-07-25 11:46:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+from urllib.error import URLError 
 streamlit.title('My Healthy Diner')
 
 streamlit.header('Todays Menu')
