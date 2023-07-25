fd27c2ea8d29c57127ebc76c25c69cc9edf6c633
rahuldodkal <140070303+rahuldodkal@users.noreply.github.com>
2023-07-25 14:41:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError 
 streamlit.title('My Healthy Diner') 
-
-
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
